7d3544233d62b374cd47e38ab17fe270c465b5fc
nexquare@Sant <sant.raj@nexquare.io>
2021-12-18 15:45:10 +0530

    first commit

---
+++ src/main/java/com/example/main/AcuatorSetUp/AcuatorSetUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.main.AcuatorSetUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuatorSetUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcuatorSetUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/main/AcuatorSetUp/AcuatorSetUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.main.AcuatorSetUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuatorSetUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
